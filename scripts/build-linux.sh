54207d7a21698e976936444a4fdb070aed93ecc4
Michael R. Furman <n6il@ocs.net>
2022-12-05 13:15:47 -0800

    Linux build script fix

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,3 +1,3 @@
-pyinstaller -p /usr/local/lib/python2.7/site-packages --hidden-import reportlab --hidden-import pillow --hidden-import paramiko  --hidden-import playsound   -- add-data ui:ui --add-data fonts:fonts -F pyDriveWire.py
+pyinstaller -p /usr/local/lib/python2.7/site-packages --hidden-import reportlab --hidden-import pillow --hidden-import paramiko  --hidden-import playsound   --add-data ui:ui --add-data fonts:fonts -F pyDriveWire.py
 pyinstaller -p /usr/local/lib/python2.7/site-packages -F pyDwCli.py
 
